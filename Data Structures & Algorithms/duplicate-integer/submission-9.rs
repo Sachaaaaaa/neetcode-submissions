36957edd13911c191274346728c217b907055d0e
Sacha Wos <66419593+Sachaaaaaa@users.noreply.github.com>
2026-06-22 22:31:25 +0200

    Add: duplicate-integer - submission-9

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.rs
@@ -0,0 +1,26 @@
+impl Solution {
+
+    /* 
+    Not optimal because if contains + insert compute two times the same hash 
+    if an element is not in the HashSet
+    
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        let mut dict = HashSet::new(); // Pas de with capacity car retour anticipé
+
+        for elem in nums {
+            if dict.contains(&elem) {
+                return true;
+            } else {
+                dict.insert(elem);
+            }
+        }
+
+        false
+    } */
+
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        let mut dict = HashSet::new(); // Pas de with capacity car retour anticipé
+
+        nums.into_iter().any(|num| !dict.insert(num))
+    }
+}
